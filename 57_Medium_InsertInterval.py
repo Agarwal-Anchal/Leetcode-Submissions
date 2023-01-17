342cac3a9b1614721f9b22228a18aec42b310f0b
Anchal Agarwal <36487172+Agarwal-Anchal@users.noreply.github.com>
2023-01-17 21:12:49 +0530

    Create 57_Medium_InsertInterval.py

---
+++ 57_Medium_InsertInterval.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insert(self, ins: List[List[int]], new_interval: List[int]) -> List[List[int]]:
+        N = len(ins)
+        i = 0
+        res = []
+        while i < N and ins[i][1] < new_interval[0]:
+            res.append(ins[i])
+            i += 1
+
+        res.append(new_interval)
+        while i < N and min(res[-1][1], ins[i][1]) >= max(ins[i][0], res[-1][0]):
+            res[-1] = min(res[-1][0], ins[i][0]), max(res[-1][1], ins[i][1])
+            i += 1
+
+        while i < N:
+            res.append(ins[i])
+            i += 1
+        
+        return res
